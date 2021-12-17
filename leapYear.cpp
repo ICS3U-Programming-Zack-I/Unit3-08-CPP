ad0b9000062841aa0fc8d2135789b4d9b8540e3d
Ubuntu <ubuntu@ip-172-31-87-46.ec2.internal>
2021-12-17 23:26:12 +0000

    leap year c++

---
+++ leapYear.cpp
@@ -0,0 +1,29 @@
+// Copyright (c)
+// 2021 Zack Isingoma
+// Creared by: Zack Isingoma
+// Created on: Dec, 17 2021.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year;
+
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                cout << year << " is a leap year.";
+            else
+                cout << year << " is not a leap year.";
+        }
+        else
+            cout << year << " is a leap year.";
+    }
+    else
+        cout << year << " is not a leap year.";
+
+    return 0;
+}
